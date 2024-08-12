05b54e3956fcad66d1b6c921a7b5e577a5eb91a7
jairolhu <j7l5h9u8@gmail.com>
2024-08-11 22:17:51 -0600

    update

---
+++ scripts/lib.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Instalar programas esenciales
+sudo pacman -S networkmanager grub efibootmgr xorg lightdm lightdm-gtk-greeter xfce4 xfce4-goodies alacritty git neofetch firefox rofi pulseaudio pavucontrol htop xdg-user-dirs zsh gvfs-mtp gvfs-gphoto2 calibre qbittorrent bc mpd ncmpcpp ntp cron adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts r rstudio timeshift ibus-mozc p7zip-gui
+
+# qtile
+# sudo pacman -S qtile nitrogen lxappearance htop python-psutil ranger feh flameshot pcmanfn-gtk3 python-iwlib
+
+xdg-user-dirs-update
+
+# Oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Horario
+timedatectl set-timezone America/Mexico_City
+timedatectl set-ntp true
+systemctl restart systemd-timesyncd
+
+# AUR
+mkdir Downloads/git
+cd Downloads/git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sri
+cd
+yay -S gtk3-patched-filechooser-icon-view glib2-patched-thumbnailer papirus-icon-theme-git
+
+# Mover archivos de configuración
+sudo rsync -a /home/jairo/dotfiles/.config /home/jairo
+sudo rsync -a /home/jairo/dotfiles/.icons /home/jairo
+sudo rsync -a /home/jairo/dotfiles/.oh-my-zsh  /home/jairo
---
+++ scripts/lib0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Chaotic AUR
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+#sudo mv /etc/pacman.conf /etc/pacman1.conf
+#sudo mv /home/jairo/dotfiles/pacman.conf /etc
+
+
