ff5f2f55b021616ae4cdfec36ea9677321da8fc4
jairolhu <j7l5h9u8@gmail.com>
2024-08-18 17:33:29 -0600

    update-script

--- scripts/starting-setup.sh
+++ scripts/starting-setup.sh
@@ -6,12 +6,13 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-su
-echo "" >> /etc/pacman.conf
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-exit
+sudo tee -a /etc/pacman.conf << EOF > /dev/null
 
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+
+# Actualizar pacman
 sudo pacman -Syu
 
 # Instalar programas esenciales
