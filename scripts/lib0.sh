591c11a2e03c232efec32bd35857a1f1d73e440e
jairolhu <j7l5h9u8@gmail.com>
2024-08-18 16:44:52 -0600

    update-script

--- scripts/lib0.sh
+++ scripts/lib0.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Chaotic AUR
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-#sudo mv /etc/pacman.conf /etc/pacman1.conf
-#sudo mv /home/jairo/dotfiles/pacman.conf /etc
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-
+su
+echo "" >> /etc/pacman.conf
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+exit
--- scripts/starting-setup.sh
+++ scripts/starting-setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Chaotic AUR
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+su
+echo "" >> /etc/pacman.conf
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+exit
+
 sudo pacman -Syu
 
 # Instalar programas esenciales
@@ -27,6 +39,7 @@ cd yay
 makepkg -sri
 cd
 #yay -S gtk3-patched-filechooser-icon-view glib2-patched-thumbnailer papirus-icon-theme-git ibus-autostart
+yay -S ibus-autostart
 
 # Mover archivos de configuración
 sudo rsync -a /home/jairo/dotfiles/.config /home/jairo
