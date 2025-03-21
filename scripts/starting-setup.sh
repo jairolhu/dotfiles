b59b700204af13e3ce857f284fbc5f4c92faa69a
jairolhu <j7l5h9u8@gmail.com>
2025-03-20 18:14:54 -0600

    updating

--- scripts/starting-setup.sh
+++ scripts/starting-setup.sh
@@ -24,8 +24,16 @@ EOF
 sudo pacman -Syu
 
 # Instalar programas esenciales
-sudo pacman -S networkmanager grub efibootmgr xorg lightdm lightdm-gtk-greeter xfce4 xfce4-goodies alacritty git neofetch firefox rofi pulseaudio pavucontrol htop xdg-user-dirs zsh gvfs-mtp gvfs-gphoto2 calibre qbittorrent bc mpd ncmpcpp ntp cron adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts r rstudio-desktop timeshift ibus-mozc p7zip-gui rsync wget
-yay -S gtk3-patched-filechooser-icon-view glib2-patched-thumbnailer papirus-icon-theme-git ibus-autostart ibus-autostart
+sudo pacman -S xorg lightdm lightdm-gtk-greeter xfce4 xfce4-goodies \
+               alacritty git neofetch firefox rofi pulseaudio pavucontrol \
+               htop xdg-user-dirs zsh gvfs-mtp gvfs-gphoto2 calibre \
+               qbittorrent bc mpd ncmpcpp ntp cron adobe-source-han-sans-jp-fonts \
+               adobe-source-han-serif-jp-fonts r rstudio-desktop timeshift ibus-mozc \
+               p7zip-gui rsync wget network-manager-applet fuse2 ntfs-3g mpv gparted \
+               yt-dlp shotcut syncthing easytag libroffice-fresh jdownloader2 \
+               noto-fonts-cjk anki vulkan-intel ffmpeg appimagelauncher \
+               simplescreenrecorder fastfetch p7zip-gui
+# yay -S gtk3-patched-filechooser-icon-view glib2-patched-thumbnailer papirus-icon-theme-git ibus-autostart
 
 # qtile
 # sudo pacman -S qtile nitrogen lxappearance htop python-psutil ranger feh flameshot pcmanfn-gtk3 python-iwlib
@@ -44,3 +52,4 @@ sudo rsync -a /home/jairo/dotfiles/.icons /home/jairo
 sudo rsync -a /home/jairo/dotfiles/.oh-my-zsh  /home/jairo
 sudo rsync -a /home/jairo/dotfiles/.zshrc /home/jairo
 sudo rsync -a /home/jairo/dotfiles/xfce4-genmon-scripts /home/jairo
+sudo rsync -a /home/jairo/dotfiles/usr/share/fonts/ /usr/share/fonts
